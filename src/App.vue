320982e29cb2f5b0b9e45333da87b794e574cb1d
kwongliegaai <a1176079417@gmail.com>
2023-05-28 17:14:32 +0800

    fix: 修复 container 导致的暗黑模式显示不全的问题

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div id="app" mx-auto h-full px-10 py-20 text-center container dark:bg-dark>
+  <div id="app" mx-auto h-full px-10 py-20 text-center dark:bg-dark>
     <router-view />
   </div>
 </template>
