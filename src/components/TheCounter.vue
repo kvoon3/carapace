9b6f45d14c24f40cb58aaa820985946340debd5a
kvoon <a1176079417@gmail.com>
2024-04-10 11:18:48 +0800

    fix(counter): numeric input mode

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -4,11 +4,13 @@ const count = ref(0)
 
 <template>
   <div flex items-center justify-center>
-    <button bg-orange btn i-carbon-add @click="count++" />
+    <button bg-orange btn i-carbon-add size-6 @click="count++" />
     <input
-      v-model="count" outline-none active:outline-none
+      v-model.number="count"
+      outline-none active:outline-none
       rounded-2 bg-slate-1 text-center dark:bg-dark py1.5
+      inputmode="numeric"
     >
-    <button bg-orange btn i-carbon-subtract @click="count--" />
+    <button size-6 bg-orange btn i-carbon-subtract @click="count--" />
   </div>
 </template>
