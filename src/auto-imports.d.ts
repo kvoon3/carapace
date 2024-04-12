04e60f92a71eaf8b8350a9876fc597c6232318b0
kvoon <a1176079417@gmail.com>
2024-04-12 15:21:31 +0800

    chore: update

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -68,6 +68,8 @@ declare global {
 declare global {
   // @ts-ignore
   export type { Component, ComponentPublicInstance, ComputedRef, ExtractDefaultPropTypes, ExtractPropTypes, ExtractPublicPropTypes, InjectionKey, PropType, Ref, VNode, WritableComputedRef } from 'vue'
+  import('vue')
   // @ts-ignore
   export type { RouteLocationRaw } from 'vue-router'
+  import('vue-router')
 }
