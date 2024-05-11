1d965c1cdc945f988f8bdd43a0fcc1b1dbc2d769
kvoon <a1176079417@gmail.com>
2024-05-11 16:40:52 +0800

    chore: update deps

--- src/logics/utils/array.ts
+++ src/logics/utils/array.ts
@@ -8,7 +8,7 @@ export function sortToFirst<T>(array: T[], finder: (item: T) => boolean) {
   array.unshift(array.splice(idx, 1)[0])
 }
 
-export function sortToIndex<T>(array: T[], finder: (item: T) => boolean, index: number) {
+export function sortToIdx<T>(array: T[], finder: (item: T) => boolean, index: number) {
   const idx = array.findIndex(finder)
   array.splice(idx, 1)
   array.splice(index, 0, array[idx])
